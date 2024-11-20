cf450b2e0cba545eb696b206ec4e8e6cade3cf6f
bootcamp <bootcampntt2024@gmail.com>
2024-11-20 14:57:31 -0500

    chore: check

--- js/services/productsApi.js
+++ js/services/productsApi.js
@@ -1,5 +1,6 @@
 import { apiService } from "./apiService.js";
 
+// es buena la separaci'on de esta url, una mejora adicional ser'ia usando variables de entorno
 const BASE_URL = "https://dummyjson.com";
 
 export const getProducts = async () => {
